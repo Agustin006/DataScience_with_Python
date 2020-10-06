8932928afc7db27e01c5b1c35767c54c7852de35
Agustin006 <64797613+Agustin006@users.noreply.github.com>
2020-10-06 15:09:16 +0200

    Prueba clase 1
    esto es una prueba mushashos

---
+++ Class 1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Clase 1
+# 
+objetivos:
+    -entender la diferentes data types.
+    -Hacer ejercicios practicos.
+    -Llevar deberes.
+# In[ ]:
+
+
+
+
